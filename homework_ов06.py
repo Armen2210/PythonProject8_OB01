8b7f3334832f8b8cfdf17d0f7c5cd49c046fe273
Армен Амбарцумян <armen0858@gmail.com>
2025-09-02 23:23:51 +0200

    Homework_ов06
    Домашнее задание ОВ06

---
+++ homework_ов06.py
@@ -0,0 +1,66 @@
+# Задание: Разработать консольную игру "Битва героев" на Python с использованием классов.
+# Общее описание:
+# Создайте простую текстовую боевую игру, где игрок и компьютер управляют героями с различными характеристиками.
+# Игра состоит из раундов, в каждом раунде игроки по очереди наносят урон друг другу, пока у одного из героев не закончится здоровье.
+#
+# Требования:
+# 1. Используйте ООП (Объектно-Ориентированное Программирование) для создания классов героев.
+# 2. Игра должна быть реализована как консольное приложение.
+#
+# Классы:
+# Класс `Hero`:
+# - Атрибуты:
+# - Имя (`name`)
+# - Здоровье (`health`), начальное значение 100
+# - Сила удара (`attack_power`), начальное значение 20
+#
+# - Методы:
+# - `attack(other)`: атакует другого героя (`other`), отнимая здоровье в размере своей силы удара
+# - `is_alive()`: возвращает `True`, если здоровье героя больше 0, иначе `False`
+#
+# Класс `Game`:
+# - Атрибуты:
+# - Игрок (`player`), экземпляр класса `Hero`
+# - Компьютер (`computer`), экземпляр класса `Hero`
+# - Методы:
+# - `start()`: начинает игру, чередует ходы игрока и компьютера, пока один из героев не умрет.
+# Выводит информацию о каждом ходе (кто атаковал и сколько здоровья осталось у противника) и объявляет победителя.
+
+
+class Hero:
+    def __init__(self, name, health=100, attack_power=20):
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power
+
+    def attack(self, other):
+        other.health -= self.attack_power
+
+    def is_alive(self):
+        return self.health > 0
+
+class Game:
+    def __init__(self, player, computer):
+        self.player = player
+        self.computer = computer
+
+    def start(self):
+        print(f"Начинается битва между {self.player.name} и {self.computer.name}.")
+        while self.player.is_alive() and self.computer.is_alive():
+            self.player.attack(self.computer)
+            print(f"{self.player.name} атакует {self.computer.name} и наносит {self.player.attack_power} урона.")
+            print(f"{self.computer.name} осталось {self.computer.health} здоровья.")
+            self.computer.attack(self.player)
+            print(f"{self.computer.name} атакует {self.player.name} и наносит {self.computer.attack_power} урона.")
+            print(f"{self.player.name} осталось {self.player.health} здоровья.")
+
+        if self.player.is_alive():
+            print(f"{self.player.name} победил!")
+        else:
+            print(f"{self.computer.name} победил!")
+
+player = Hero("Игрок")
+computer = Hero("Компьютер")
+
+game = Game(player, computer)
+game.start()
