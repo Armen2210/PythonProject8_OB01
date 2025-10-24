b037f154a2a5dc3c693117e6d14e47cdaa9a1d2c
Армен Амбарцумян <armen0858@gmail.com>
2025-10-24 21:55:21 +0200

    add new file AZ01_Numpy_Pandas

---
+++ AZ01_Numpy_Pandas.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+# data = {
+#     'Name': ['John', 'Anna', 'Peter', 'Linda'],
+#     'Age': [28, 22, 34, 42],
+#     'City': ['New York', 'Paris', 'London', 'Berlin']
+# }
+#
+# df = pd.DataFrame(data)
+# print(df)
+
+# df = pd.read_csv("csv_files/World-happiness-report-2024.csv")
+# print(df.head(2)) # выводит первые строки дата фрэйма(по умолчанию 5)
+# print(df.tail(3)) # выводит последние строки дата фрэйма
+# print(df.info()) # выводит информацию о дата фрэйме
+# print(df.describe()) # выводит основные статистические данные
+# print(df['Country name']) # выводит данные по выбранному столбцу, первые и последние пять
+# print(df[['Country name', 'Regional indicator']]) # выводит данные по выбранным столбцам, для этого двойные скобки
+# print(df.loc[56]) # выводит все данные по выбранной строке
+# print(df.loc[56, 'Social support']) # выводит выбранные данные по указанной строке
+# print(df[df['Social support'] > 1.400]) # выводит данные по выбранному столбцу со значением больше 1.400
+
+
+df = pd.read_csv("csv_files/hh.csv")
+
+# df['Test'] = [new for new in range(29)] # добавили столбец
+# print(df)
+
+# df.drop('Test', axis=1, inplace=True) #  удаляем объект, который указываем; axis=1 - удаление столбцов, axis=0 - удаление строк; inplace=True - изменения вносятся в исходный дата фрэйм, inplace=False - возвращается измененный дата фрэйм, оригинальный не меняется
+# print(df)
+
+df.drop('Test', axis=1, inplace=True) #  удаляем объект, который указываем; axis=1 - удаление столбцов, axis=0 - удаление строк; inplace=True - изменения вносятся в исходный дата фрэйм, inplace=False - возвращается измененный дата фрэйм, оригинальный не меняется
+print(df)
